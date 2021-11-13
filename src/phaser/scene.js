cf607a06f3887f8331cda02452f632ddaa70e259
Felipe LIncoln <felipelincoln@usp.br>
2021-11-13 15:32:32 -0300

    add drag event

--- src/phaser/scene.js
+++ src/phaser/scene.js
@@ -31,8 +31,39 @@ export default class MapScene extends Scene {
     }
 
     create() {
-
         this.beasties_map = this.add.image(0, 0, "map").setInteractive();
+        this.beasties_map.isBeingDragged = false
+        this.beasties_map.movingSpeed = 0
+
+        this.input.setDraggable(this.beasties_map)
+        this.input.dragDistanceThreshold = 16
+
+        this.input.on('dragstart', (pointer, gameObject) => {
+          this.beasties_map.isBeingDragged = true
+        })
+
+        this.input.on('dragend', (pointer, gameObject) => {
+          this.beasties_map.isBeingDragged = false
+        })
+
+        this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
+          gameObject.x = dragX
+          gameObject.y = dragY
+          this.beasties_map.movingSpeed = 0
+
+          for (let i in this.adventures) {
+              this.adventures[i]['object'].x = dragX + adventures[i]['position']['x']
+              this.adventures[i]['object'].y = dragY + adventures[i]['position']['y']
+          }
+          for (let i in this.dungeons) {
+              this.dungeons[i]['object'].x = dragX + dungeons[i]['position']['x']
+              this.dungeons[i]['object'].y = dragY + dungeons[i]['position']['y']
+          }
+
+          this.test_button.x = dragX + 300
+          this.test_button.y = dragY + 300
+
+        })
 
         this.adventures = adventures;
         for (let i in this.adventures) {
@@ -44,18 +75,17 @@ export default class MapScene extends Scene {
             this.dungeons[i]['object'] = this.add.image(dungeons[i]['position']['x'], dungeons[i]['position']['y'], dungeons[i]['key'])
         }
 
-        const test_button = this.add.text(300, 300, 'Open Dungeon', { fill: '#0f0' });
-        test_button.setInteractive();
-        test_button.on('pointerdown', function (){
+        this.test_button = this.add.text(300, 300, 'Open Dungeon', { fill: '#0f0' });
+        this.test_button.setInteractive();
+        this.test_button.on('pointerdown', function (){
             store.commit('pve_set_current_dungeon', 1)
             store.commit('pve_show_dungeon_popup')
         })
+        console.log(this.beasties_map)
 
-        console.log(this.adventures)
     }
 
     update() {
-
         let player_level = store.getters.get_player['level']
         // Grayscale adventures with level < player level
 
